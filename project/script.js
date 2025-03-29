b0ff48c0af5777dc43335e39ee50910ec8c5efe5
Felix Freund <f.freund1@students.htl-leonding.ac.at>
2025-03-29 01:56:36 +0100

    slideshow prototypV2 + new background design

--- project/script.js
+++ project/script.js
@@ -1,24 +1,62 @@
-let index = 0;
-        const slides = document.querySelectorAll(".slide");
-        
-        function showSlide(i, direction) {
-            slides.forEach(slide => {
-                slide.style.opacity = "0";
-                slide.style.transform = `translateX(${direction * 100}%)`;
-            });
-            slides[i].style.opacity = "1";
-            slides[i].style.transform = "translateX(0)";
-        }
-        
-        function changeSlide(step) {
-            let direction = step > 0 ? 1 : -1;
-            index = (index + step + slides.length) % slides.length;
-            showSlide(index, direction);
-        }
-        
-        function autoSlide() {
-            changeSlide(1);
-            setTimeout(autoSlide, 5000);
-        }
-        
-        setTimeout(autoSlide, 5000);
+let timerLeft
+let timerRight
+
+let images = [
+    "./img/Album1.webp",
+    "./img/Album2.jpeg",
+    "./img/album3.jpg",
+    "./img/album4.jpg"
+]
+
+function nextPictureRight(){
+    let currentPicture = images[3];
+   for(let i=3; i > 0; i--){
+        images[i] = images[i -1]
+    }
+    images[0] = currentPicture;
+    document.getElementById('img2').innerHTML = `<img id="img2"src="${images[0]}" alt="">`
+    document.getElementById('img3').innerHTML = `<img id="img3"src="${images[1]}" alt="">`
+    document.getElementById('img4').innerHTML = `<img id="img4"src="${images[2]}" alt="">`
+    clearInterval(timerRight)
+    clearInterval(timerLeft)
+    timerRight = setInterval(nextPictureRightAuto,6000)
+}
+
+function nextPictureRightAuto(){
+    let currentPicture = images[3];
+   for(let i=3; i > 0; i--){
+        images[i] = images[i -1]
+    }
+    images[0] = currentPicture;
+    document.getElementById('img2').innerHTML = `<img id="img2"src="${images[0]}" alt="">`
+    document.getElementById('img3').innerHTML = `<img id="img3"src="${images[1]}" alt="">`
+    document.getElementById('img4').innerHTML = `<img id="img4"src="${images[2]}" alt="">`
+    
+}
+
+function nextPictureLeft(){
+   let currentPicture = images[0];
+   for(let i=0; i < 3; i++){
+       images[i] = images[i+1]
+    }
+    images[3] = currentPicture;
+    document.getElementById('img2').innerHTML = `<img id="img2"src="${images[0]}" alt="">`
+    document.getElementById('img3').innerHTML = `<img id="img3"src="${images[1]}" alt="">`
+    document.getElementById('img4').innerHTML = `<img id="img4"src="${images[2]}" alt="">`
+    clearInterval(timerRight)
+    clearInterval(timerLeft)
+    timerLeft = setInterval(nextPictureLeftAuto,6000)
+    
+}
+function nextPictureLeftAuto(){
+    let currentPicture = images[0];
+    for(let i=0; i < 3; i++){
+         images[i] = images[i+1]
+     }
+     images[3] = currentPicture;
+     document.getElementById('img2').innerHTML = `<img id="img2"src="${images[0]}" alt="">`
+     document.getElementById('img3').innerHTML = `<img id="img3"src="${images[1]}" alt="">`
+     document.getElementById('img4').innerHTML = `<img id="img4"src="${images[2]}" alt="">`
+   
+ }
+ 
